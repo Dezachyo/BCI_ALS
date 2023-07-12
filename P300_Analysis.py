15b53e95f18c0df758f0da23bfd95822b31637a7
or dez <dezachyo@gmail.com>
2023-07-12 17:46:26 +0300

    notebook convert to pytohn, 3 classes oddball adjusted (prepro, p300 analysis)

---
+++ P300_Analysis.py
@@ -0,0 +1,80 @@
+#%% -----------------------------Imports ---------------------------
+import pyxdf
+import PyQt5
+import mne
+import numpy as np
+import pathlib
+import mne
+import pickle
+import matplotlib
+import matplotlib.pyplot as plt
+# For interactive plots
+from IPython import get_ipython
+get_ipython().run_line_magic('matplotlib', 'qt')
+from subfunctions import *
+# %%
+#processed_file_name = 'Or_1304'
+processed_file_name = 'sub-Synt_ses-Synt_task-3_Class_run-001_eeg'
+
+current_path = pathlib.Path().absolute()  
+data_fname = current_path /'Data'/'Processed Data'/ (processed_file_name + '_Processed.fif')
+epochs = mne.read_epochs(data_fname)
+epochs
+
+# %% -------------------------Create Evokeds dict for plotting
+
+target_evoked = epochs['Target Trial'].average()
+nontarget_evoked = epochs['Non-Target Trial'].average()
+distractor_evoked = epochs['Distractor Trial'].average()
+
+evokeds = dict(nontarget=nontarget_evoked, target=target_evoked,distractor =distractor_evoked)
+
+
+# %% --------------------- Verify cleaned epochs (optional)
+fig, ax = plt.subplots(3,2)
+
+epochs['Target Trial'].plot_image(picks='eeg', combine='mean',axes=ax[:,0],title="Target")
+epochs['Non-Target Trial'].plot_image(picks='eeg', combine='mean',axes=ax[:,1],title="Standard")
+
+plt.show()
+
+
+# %% --------------------------- Evoked in diffrent plots
+fig, ax = plt.subplots(2)
+
+P300_window = [0.25,0.4]
+par_picks = ['CP1','CP2'] # to plot only parietal
+
+
+target_evoked.plot(gfp=True,
+    highlight=P300_window,axes=ax[0],titles='odd')
+
+nontarget_evoked.plot(gfp=True,
+    highlight=P300_window,axes=ax[1],titles='standard')
+
+plt.show()
+
+
+# %% ----------------------------- Compare Evoked (P300 Effect) --------------------------
+
+mne.viz.plot_compare_evokeds(evokeds ,combine='mean',show_sensors= True,vlines= [0.3] )
+
+# %% -------------------------------- ERP Topoplots ----------------------------------------
+
+mne.viz.plot_compare_evokeds(evokeds,axes='topo',vlines=[0,0.3])
+
+# %% 
+
+Difference_wave = mne.combine_evoked([target_evoked,nontarget_evoked],weights=[1,-1])
+mne.viz.plot_compare_evokeds(dict(nontaget=nontarget_evoked, target=target_evoked, difference=Difference_wave),axes='topo',vlines=[0,0.3])
+
+mne.viz.plot_compare_evokeds(evokeds,combine='mean',show_sensors= True,vlines= [0.3] )
+
+# %% ------------------------ Joint plots
+par_picks = ['CP1','CP2'] # to plot only parietal
+
+odd_evoked =  epochs['Target Trial'].average()
+stand_evoked =  epochs['Standard Trial'].average()
+
+
+odd_evoked.plot_joint(picks='eeg',times=[0,0.1,0.2,0.3])
---
+++ Preprocessing.py
@@ -0,0 +1,73 @@
+#%% -----------------------------Imports ---------------------------
+
+# Some standard pythonic imports
+import warnings
+warnings.filterwarnings('ignore')
+import os,numpy as np,pandas as pd
+from collections import OrderedDict
+import seaborn as sns
+import matplotlib
+from matplotlib import pyplot as plt
+import pathlib
+from os import listdir
+from os.path import isfile, join
+import pyxdf
+import PyQt5
+import pickle
+# MNE functions
+import mne
+from mne import Epochs,find_events
+# Custom function
+from subfunctions import *
+# For interactive plots
+from IPython import get_ipython
+get_ipython().run_line_magic('matplotlib', 'qt')
+# %% -------------------- Read Data -----------------------------------
+
+fname = 'sub-Synt_ses-Synt_task-3_Class_run-001_eeg.xdf'
+current_path = pathlib.Path().absolute()  
+
+## Recording to XDF
+current_path = pathlib.Path().absolute()  
+data_fname = current_path /'Data'/fname
+# Add annotation from events
+raw,events = read_xdf(fname = data_fname)
+raw = add_annot(raw, events)
+# %% --------------------- Bandpass filtering ---------------------------
+[LowPass, HighPass] = 1 , 40
+
+LowPass = float(LowPass)
+HighPass = float(HighPass)
+filter_method = 'iir'
+
+selected_filter = BandwidthFilter(LowPass, HighPass, filter_method)
+Raw_Filtered = raw.filter(LowPass, HighPass, method=filter_method)
+
+print (f'Bandpass filter {filter_method} [{int(LowPass)}-{int(HighPass)} Hz]')
+
+# %% --------------------- Remove bad channels-------------------------------
+
+Raw_Filtered.plot()
+# %% --------------------- Epoching-------------------------------
+Raw_Filtered.drop_channels(Raw_Filtered.info['bads'])
+Raw_Filtered.set_eeg_reference(ref_channels='average')
+events_from_annot, event_dict = mne.events_from_annotations(Raw_Filtered)
+if 'Break' in event_dict:
+    event_dict.pop('Break')
+epochs = mne.Epochs(Raw_Filtered, events_from_annot, tmin=-0.2, tmax=0.5, event_id=event_dict,detrend=1,baseline= (-0.2,0), preload = True)
+# %% --------------------- Drop Noisy Epochs-------------------------------
+epochs.plot()
+# %% --------------------- Verify cleaned epochs (optional)
+fig, ax = plt.subplots(3,2)
+
+epochs['Target Trial'].plot_image(picks='eeg', combine='mean',axes=ax[:,0],title="Target")
+epochs['Non-Target Trial'].plot_image(picks='eeg', combine='mean',axes=ax[:,1],title="Non-Target Trial")
+# %%
+fif_export_path = current_path/'Data'/'Processed Data'/f'{fname[:-4]}_Processed.fif'
+epochs.save(fif_export_path,overwrite=True)
+bwfilter_export_path = current_path/'Data'/'Processed Data'/f'{fname[:-4]}_Filter'
+
+with open(bwfilter_export_path, "wb") as file:
+    pickle.dump(selected_filter, file)
+
+# %%
